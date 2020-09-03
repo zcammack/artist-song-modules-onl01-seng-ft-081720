fb3ab960d29eb47f7599a74ecfc5a07968552240
Zackary Cammack <zack.cammack@gmail.com>
2020-09-03 17:34:53 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -8,37 +8,37 @@ class Artist
 
   extend Memorable
 
-  def initialize
-    @@artists << self
-    @songs = []
-  end
-
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
-
-  def self.all
-    @@artists
-  end
-
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
-  end
-
-  def add_song(song)
-    @songs << song
-    song.artist = self
-  end
-
-  def add_songs(songs)
-    songs.each { |song| add_song(song) }
-  end
-
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  # def initialize
+  #   @@artists << self
+  #   @songs = []
+  # end
+  #
+  # def self.find_by_name(name)
+  #   @@artists.detect{|a| a.name == name}
+  # end
+  #
+  # def self.all
+  #   @@artists
+  # end
+  #
+  # def self.reset_all
+  #   self.all.clear
+  # end
+  #
+  # def self.count
+  #   self.all.count
+  # end
+  #
+  # def add_song(song)
+  #   @songs << song
+  #   song.artist = self
+  # end
+  #
+  # def add_songs(songs)
+  #   songs.each { |song| add_song(song) }
+  # end
+  #
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
 end
