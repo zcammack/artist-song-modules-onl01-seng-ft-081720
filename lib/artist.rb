0a0bcd73cf20536057f6f968ff089e7771359603
Zackary Cammack <zack.cammack@gmail.com>
2020-09-03 18:11:44 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -14,21 +14,9 @@ include Paramable
     @songs = []
   end
 
-  # def self.find_by_name(name)
-  #   @@artists.detect{|a| a.name == name}
-  # end
-
   def self.all
     @@artists
   end
-
-  # def self.reset_all
-  #   self.all.clear
-  # end
-
-  # def self.count
-  #   self.all.count
-  # end
 
   def add_song(song)
     @songs << song
@@ -38,8 +26,4 @@ include Paramable
   def add_songs(songs)
     songs.each { |song| add_song(song) }
   end
-
-  # def to_param
-  #   name.downcase.gsub(' ', '-')
-  # end
 end
