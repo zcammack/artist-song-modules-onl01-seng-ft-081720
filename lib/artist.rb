6e80f21cd38411e0b97efa091da078d05420ac9e
Zackary Cammack <zack.cammack@gmail.com>
2020-09-03 17:57:47 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -13,9 +13,9 @@ extend Findable
     @songs = []
   end
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
+  # def self.find_by_name(name)
+  #   @@artists.detect{|a| a.name == name}
+  # end
 
   def self.all
     @@artists
