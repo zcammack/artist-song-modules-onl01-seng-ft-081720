0932ae7168c60a499c236c9e97e4b0eec2da4cf0
Zackary Cammack <zack.cammack@gmail.com>
2020-09-03 18:22:46 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 class Artist
 extend Memorable::ClassMethods
-extend Memorable::InstanceMethods
+include Memorable::InstanceMethods
 extend Findable
 include Paramable
   attr_accessor :name
