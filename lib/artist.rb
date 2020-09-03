8fcfe59888fe99d8a9ec5dc2e9f9360218ba498c
Zackary Cammack <zack.cammack@gmail.com>
2020-09-03 17:53:12 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -16,17 +16,17 @@ extend Memorable
     @@artists.detect{|a| a.name == name}
   end
 
-  # def self.all
-  #   @@artists
-  # end
-  #
+  def self.all
+    @@artists
+  end
+
   # def self.reset_all
   #   self.all.clear
   # end
 
-  def self.count
-    self.all.count
-  end
+  # def self.count
+  #   self.all.count
+  # end
 
   def add_song(song)
     @songs << song
