cc252b1b98de14f5a28d5e4e35d75f6225a99a6c
Zackary Cammack <zack.cammack@gmail.com>
2020-09-03 17:41:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -20,13 +20,13 @@ class Song
     @@songs
   end
 
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
-  end
+  # def self.reset_all
+  #   self.all.clear
+  # end
+  # 
+  # def self.count
+  #   self.all.count
+  # end
 
   def artist=(artist)
     @artist = artist
