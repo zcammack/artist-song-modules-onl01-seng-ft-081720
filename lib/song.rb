1b5d04f919fa9451a68ea05f09bae99467d799ed
Zackary Cammack <zack.cammack@gmail.com>
2020-09-03 17:50:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,12 +1,12 @@
 require 'pry'
 
 class Song
+extend Memorable
+
   attr_accessor :name
   attr_reader :artist
 
   @@songs = []
-
-  extend Memorable
 
   def initialize
     @@songs << self
@@ -23,7 +23,7 @@ class Song
   # def self.reset_all
   #   self.all.clear
   # end
-  # 
+  #
   # def self.count
   #   self.all.count
   # end
